39460856212debfdb74475b110af161987eb0914
sbx_user1059 <sbx_user1059@ip-10-128-147-186.eu-west-2.compute.internal>
2018-11-28 11:31:48 +0000

    Wed Nov 28 11:31:48 UTC 2018

--- challenges/sum.py
+++ challenges/sum.py
@@ -1,17 +1,18 @@
 def sum (int1, int2):
-    if type(int1) not in [int] or type(int2) not in [int]:
-        raise TypeError("The arguments must be integer")
-    if (int1 < 0) or (int1 > 100) or (int2 < 0) or (int2 > 100):
-        raise valueError("The arguments must be in range 0-100")
+#    if type(int1) not in [int] or type(int2) not in [int]:
+        #raise TypeError("The arguments must be integer")
+    #if (int1 < 0) or (int1 > 100) or (int2 < 0) or (int2 > 100):
+        #raise valueError("The arguments must be in range 0-100")
     return int1+int2
 
 #test function
 
-#for i in range(1,100,1):
-    #print(i, sum(i,1))
+for i in range(0,100,1):
+    print(i, sum(i,1))
 
 #print(i, sum( -1,0))
 
 #test function - type error
 #print(i, sum('one',1))
+
 

5119aef2bbff998b971493cfed26dc8370ac27c5
sbx_user1059 <sbx_user1059@ip-10-128-147-186.eu-west-2.compute.internal>
2018-11-28 11:19:07 +0000

    Wed Nov 28 11:19:07 UTC 2018

---
+++ challenges/sum.py
@@ -0,0 +1,10 @@
+def sum (int1, int2):
+    if type(int1) not in [int] or type(int2) not in [int]:
+        raise TypeError("The arguments must be integer")
+    return int1+int2
+
+#test function
+
+for i in range(1,100,1):
+    print(i, sum(i,1))
+    print(i, sum('one',1))

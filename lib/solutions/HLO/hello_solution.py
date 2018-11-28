09de0dae7a0dd5770a210516416e4330c6861b56
sbx_user1059 <sbx_user1059@ip-10-128-195-72.eu-west-2.compute.internal>
2018-11-28 14:05:08 +0000

    Wed Nov 28 14:05:08 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,12 +1,13 @@
 # friend_name = unicode string
 def hello(friend_name):
-    print "Hello, World!"
+    return "Hello, World!"
 
 
 #test function
 
-hello("Craftsman")
-hello("Mr. X")
+#print hello("Craftsman")
+#print hello("Mr. X")
+
 
 
 

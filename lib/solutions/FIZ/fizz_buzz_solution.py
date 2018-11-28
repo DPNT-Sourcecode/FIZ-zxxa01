7b01950c52e73e3bfe080a18968705fa5625339d
sbx_user1059 <sbx_user1059@ip-10-128-195-72.eu-west-2.compute.internal>
2018-11-28 15:19:52 +0000

    Wed Nov 28 15:19:52 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,23 +1,27 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
+
 	def isDivThree(n):
 	    if (n%3) == 0:
-		  
-     fcheck=divmod(number,3)
-     bcheck=divmod(number,5)
-     if (fcheck[1]==0) and (bcheck[1]==0):
- 	op="fizz buzz"
-     elif (fcheck[1]==0) and (bcheck[1]!=0):
-	op="fizz"
-     elif (fcheck[1])!=0 and (bcheck[1]==0):
-	op="buzz"
-     else:
-	   op=number
-     if (number==0):
-	op=number
-     return  op 
+	        return  True 	  
+	    else:
+                 return False
 
-#test function 
-#for i in range (-4,16):
-#	print(i,fizz_buzz(i))
+	def isDivFive(n):
+	    if (n%5) == 0:
+	        return  True 	  
+	    else:
+                 return False
+
+if isDivThree(number):
+    return 'fizz'
+elif isDivFive(number):
+    return 'buzz'
+else:
+     return number
+
+test function 
+for i in range (-4,16):
+	print(i,fizz_buzz(i))
+
 

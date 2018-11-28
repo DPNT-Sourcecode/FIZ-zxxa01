5c79e7763113f1321946d16c10462f913e844ec2
sbx_user1059 <sbx_user1059@ip-10-128-195-72.eu-west-2.compute.internal>
2018-11-28 16:02:33 +0000

    Wed Nov 28 16:02:33 UTC 2018

--- lib/solutions/FIZ/dc.py
+++ lib/solutions/FIZ/dc.py
@@ -1,9 +1,17 @@
 def dc(number):
     nstr=str(number)
     for i, d in enumerate(nstr):
-	print i,d
+	if i == 0:
+	    prev_d = d
+	if prev_d = d:
+	   isSame = True
+	   prev_d = d
+	else:
+	   return False
 
 
 #test function
 
-dc(654)
+print dc(654)
+print dc(333)
+

9abaf11a7c3620490eeca78b0525a17daf6af137
sbx_user1059 <sbx_user1059@ip-10-128-195-72.eu-west-2.compute.internal>
2018-11-28 16:08:33 +0000

    Wed Nov 28 16:08:33 UTC 2018

--- lib/solutions/FIZ/dc.py
+++ lib/solutions/FIZ/dc.py
@@ -1,13 +1,13 @@
 def dc(number):
     nstr=str(number)
     for i, d in enumerate(nstr):
-	print(i,d)
+	print('i=',i,"d=",d)
 	if i == 0:
 	    prev_d = d
 	if prev_d == d:
 	   isSame = True
 	   prev_d = d
-	   print(i,d,prev_d)
+	   print('i=',i,"d=",d, 'prev_d=',' prev_d')
 	else:
 	   return False
 
@@ -18,5 +18,6 @@ def dc(number):
 
 print dc(654)
 print dc(333)
+
 
 

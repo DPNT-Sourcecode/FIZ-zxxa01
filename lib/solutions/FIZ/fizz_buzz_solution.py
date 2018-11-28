190d8f93dfc341761f04c13eff2b44465d5cdf7c
sbx_user1059 <sbx_user1059@ip-10-128-195-72.eu-west-2.compute.internal>
2018-11-28 14:26:03 +0000

    Wed Nov 28 14:26:03 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,3 +1,18 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    raise NotImplementedError()
+     fcheck=divmod(number,3)
+     bcheck=divmod(number,5)
+     if (fcheck[1]==0) and (bcheck[1]==0):
+ 	op="fizz buzz"
+	elif (fcheck[1]==0) and (bcheck[1]!=0) :
+	    op="fizz"
+	    elif (fcheck[1]!=0 and bcheck[1]==0):
+	        op="buzz"
+        else op=number
+    return = op 
+
+#test function 
+
+print fizz_buzz(10)
+
+

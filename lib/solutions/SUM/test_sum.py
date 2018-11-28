c0f4052dead7d05389a27c7c865477419ea278f6
sbx_user1059 <sbx_user1059@ip-10-128-195-72.eu-west-2.compute.internal>
2018-11-28 13:32:11 +0000

    Wed Nov 28 13:32:11 UTC 2018

--- lib/solutions/SUM/test_sum.py
+++ lib/solutions/SUM/test_sum.py
@@ -3,7 +3,8 @@ import sum_solution
 
 class TestCompute(unittest.TestCase):
     def testSum(self):
-	selfAssertEqual(compute(0,1),1)
-	selfAssertEqual(compute(0,0),0)
+	self.assertEqual(sum_Solution.compute(0,1),1)
+	self.assertEqual(sum_Solution.compute(0,0),0)
+
 
 

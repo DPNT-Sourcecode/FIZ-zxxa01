485cb5875575ff03b3c830266df44069e3d537c2
sbx_user1059 <sbx_user1059@ip-10-128-195-72.eu-west-2.compute.internal>
2018-11-28 12:59:10 +0000

    Wed Nov 28 12:59:10 UTC 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if type(int1) not in [int] or type(int2) not in [int]:
+    if type(x) not in [int] or type(y) not in [int]:
         raise TypeError("The arguments must be integer")
-    if (int1 < 0) or (int1 > 100) or (int2 < 0) or (int2 > 100):
+    if (x < 0) or (x > 100) or (y < 0) or (y > 100):
         raise valueError("The arguments must be in range 0-100")
-    return int1+int2
+    return x+y
+

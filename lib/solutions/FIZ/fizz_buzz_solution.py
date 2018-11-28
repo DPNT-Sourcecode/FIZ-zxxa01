324ef22800598da65e3e5747b3fd33e81566ed9c
sbx_user1059 <sbx_user1059@ip-10-129-30-235.eu-west-2.compute.internal>
2018-11-28 17:08:06 +0000

    Wed Nov 28 17:08:06 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -7,13 +7,13 @@ def fizz_buzz(number):
     	    nstr=str(number)
 	    prev=False
     	    for i in range(len(nstr)):
-        	print('i=',i,"nstr[i]=",nstr[i])
+        	#print('i=',i,"nstr[i]=",nstr[i])
         	if i == 0:
             	    prev = nstr[i]
         	elif prev == nstr[i]:
            	   isSame = True
            	   prev = nstr[i]
-           	   print('i=',i,"d=",nstr[i], 'prev_d=',' prev_d')
+           	   #print('i=',i,"d=",nstr[i], 'prev_d=',' prev_d')
         	else:
            	   return False
 
@@ -54,6 +54,8 @@ def fizz_buzz(number):
 
 	if (isDivFive(number) or containsFive(number)) and (isDivThree(number) or containsThree(number)) :
     	    op= 'fizz buzz'
+        if (isDeluxe(number)):
+	    op=op+' deluxe'
 
         if number==0:
 	    op= number
@@ -65,9 +67,13 @@ def fizz_buzz(number):
 #	print(i,fizz_buzz(i))
 
 print (15, fizz_buzz(15))
+print (33, fizz_buzz(33))
 print (30, fizz_buzz(30))
 print (465, fizz_buzz(465))
 print (444, fizz_buzz(465))
+print (555, fizz_buzz(555))
+print (500, fizz_buzz(500))
+
 
 
 
